Fixing bug in the local batch weight calculation

Transfer_Learning/LearningModels.py
@@ -122,7 +122,7 @@ class LearningModels():
                 if self.args.classweight_type == 'batch':
                     #Computing class weights to mitigate the imabalance between classes
                     labels_sum = tf.reduce_sum(labels_, [0, 1, 2])/tf.constant(images.shape[0] * self.args.crop_size * self.args.crop_size, dtype = tf.float32)
-                    class_weights = tf.multiply((1-labels_sum) * 10 array)
+                    class_weights = tf.multiply((1-labels_sum) * 10, array)
                 else:
                     class_weights = tf.multiply(self.dataset.class_weights, array)
                 self.train_step(images, labels_, class_weights)