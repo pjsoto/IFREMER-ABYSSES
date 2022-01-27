326a355bf7a8bb84d75dc83253fdad60433ffc7a
pjsoto <pedrosoto423@gmail.com>
2022-01-27 05:34:33 -0300

    Fixing bug in Augmenter.py. Specifically with the crop dimensions

--- Transfer_Learning/Augmenter.py
+++ Transfer_Learning/Augmenter.py
@@ -9,7 +9,7 @@ class Augmenter():
     def __init__(self, args):
         self.args = args
         self.seq = iaa.Sequential([
-                   iaa.CropToFixedSize(width=self.args.crop_size_rows, height=self.args.crop_size_cols),
+                   iaa.CropToFixedSize(width=self.args.crop_size_cols, height=self.args.crop_size_rows),
                    iaa.Sharpen((0.0, 1.0)),
                    iaa.Affine(rotate=(-45, 45))
                    ])
