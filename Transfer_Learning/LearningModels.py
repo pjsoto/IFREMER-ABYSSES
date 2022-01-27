8ab38e972e9bde1a3d3c62691368bd1254ff5e67
pjsoto <pedrosoto423@gmail.com>
2022-01-27 06:55:51 -0300

    Fixing bug in LearningModel.py

--- Transfer_Learning/LearningModels.py
+++ Transfer_Learning/LearningModels.py
@@ -123,7 +123,8 @@ class LearningModels():
                 images = batch["image"]
                 labels = batch["label"]
                 #Hot encoding the labels
-                print(np.shape(labels))
+                print(np.max(labels))
+                print(np.min(labels))
                 labels_ = tf.keras.utils.to_categorical(labels, self.dataset.classes)
                 array = tf.constant(1, shape = [images.shape[0], self.args.crop_size_rows, self.args.crop_size_cols, self.args.classes], dtype = tf.float32)
                 if self.args.classweight_type == 'batch':
