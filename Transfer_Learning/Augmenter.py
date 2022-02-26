24735185dbd1102c1d4c9491504c3d264912cb9c
pjsoto <pedrosoto423@gmail.com>
2022-02-26 04:32:51 -0300

    The changes here were made aiming at enabling proper ways of doing data augmentation

--- Transfer_Learning/Augmenter.py
+++ Transfer_Learning/Augmenter.py
@@ -10,9 +10,12 @@ class Augmenter():
         self.args = args
         if self.args.phase == 'train':
             self.ss_seq = iaa.Sequential([
-                       iaa.CropToFixedSize(width=self.args.crop_size_cols, height=self.args.crop_size_rows),
-                       iaa.Sharpen((0.0, 1.0)),
-                       iaa.Affine(rotate=(-45, 45))
+                          iaa.CropToFixedSize(width=self.args.crop_size_cols, height=self.args.crop_size_rows),
+                          iaa.Rot90((1,3)),
+                       ])
+            self.ic_seq = iaa.Sequential([
+                          iaa.size.Resize({"height": self.args.crop_size_rows, "width": self.args.crop_size_cols}),
+                          iaa.Rot90((1,3)),
                        ])
         if self.args.phase == 'test':
             if self.args.testcrop_size_cols is not None and self.args.testcrop_size_rows is not None:
@@ -23,15 +26,20 @@ class Augmenter():
                             iaa.size.Resize({"height": self.args.testcrop_size_rows, "width": self.args.testcrop_size_cols})
                             ])
 
-    def apply_ss_augmentations(self, image, label):
+    def apply_augmentations_train(self, image, label):
         image_numpy = image.numpy()
         label_numpy = label.numpy()
-        segmap = SegmentationMapsOnImage(label_numpy, shape=image_numpy.shape)
-        image_reshape = image_numpy.reshape((1, image_numpy.shape[0], image_numpy.shape[1], image_numpy.shape[2]))
-        images_aug, labels_aug = self.ss_seq(images = image_reshape, segmentation_maps = segmap)
-        return (images_aug[0], labels_aug.get_arr())
+        if self.args.train_task == 'Semantic_Segmentation':
+            segmap = SegmentationMapsOnImage(label_numpy, shape=image_numpy.shape)
+            image_reshape = image_numpy.reshape((1, image_numpy.shape[0], image_numpy.shape[1], image_numpy.shape[2]))
+            images_aug, labels_aug = self.ss_seq(images = image_reshape, segmentation_maps = segmap)
+            return (images_aug[0], labels_aug.get_arr())
+        if self.args.train_task == 'Image_Classification':
+            image_reshape = image_numpy.reshape((1, image_numpy.shape[0], image_numpy.shape[1], image_numpy.shape[2]))
+            images_aug = self.ic_seq(images = image_reshape)
+            return (images_aug[0], label_numpy)
 
-    def apply_ci_augmentations(self, image):
+    def apply_augmentations_test(self, image):
         image_numpy = image.numpy()
         image_reshape = image_numpy.reshape((1, image_numpy.shape[0], image_numpy.shape[1], image_numpy.shape[2]))
         if self.args.test_task_level == 'Pixel_Level':
