5f77eb1331119e8e36a32ad891fab2c75eea74f8
pjsoto <pedrosoto423@gmail.com>
2022-02-26 05:11:28 -0300

    Configuring the first experiment on Datarmor

--- Transfer_Learning/runningScripts.py
+++ Transfer_Learning/runningScripts.py
@@ -33,19 +33,19 @@ if args.phase == 'Train':
     if args.running_in == 'Datarmor_Interactive':
         Schedule.append("python TrainLearningModel.py --train_task Image_Classification --learning_model CNN --backbone_name vgg16 "
                         "--crop_size_rows 4000 --crop_size_cols 6000 --labels_type multiple_labels --learning_ratedecay True --lr 0.001 --gamma 2.0 --alpha 4.0 --optimizer SGD --batch_size 5 --runs 1 "
-                        "--epochs 400 --phase train --loss weighted_binary_crossentropy --checkpoint_name Prove "
+                        "--epochs 400 --phase train --loss weighted_binary_crossentropy --checkpoint_name VGG16_F1 "
                         "--dataset_name OTUSIFREMER_IMAGELABEL --class_grouping False --classweight_type global --csvfile_name OTUS_Image_Classification_F1.csv "
-                        "--dataset_csv_main_path D:/DATA/Pedro_Work/IFREMER_Work/DATA/IFREMER_OTUS/BIGLI_CSVs/ "
-                        "--dataset_main_path /datawork/DATA/SUIM/train_val/train_val/ "
+                        "--dataset_csv_main_path /datawork/pjsotove/DATA/CSVs/OTUS_2018/ "
+                        "--dataset_main_path /datawork/pjsotove/DATA/OTUS_2018_Doneesbrutes_Montsegur/ "
                         "--checkpoints_main_path /datawork/EXPERIMENTS/")
 
     if args.running_in == 'Datarmor_PBS':
         Schedule.append("python $HOME/CODE/IFREMER-ABYSSES/Transfer_Learning/TrainLearningModel.py --train_task Image_Classification --learning_model CNN --backbone_name vgg16 "
                         "--crop_size_rows 4000 --crop_size_cols 6000 --labels_type multiple_labels --learning_ratedecay True --lr 0.001 --gamma 2.0 --alpha 4.0 --optimizer SGD --batch_size 5 --runs 1 "
-                        "--epochs 400 --phase train --loss weighted_binary_crossentropy --checkpoint_name Prove "
+                        "--epochs 400 --phase train --loss weighted_binary_crossentropy --checkpoint_name VGG16_F1 "
                         "--dataset_name OTUSIFREMER_IMAGELABEL --class_grouping False --classweight_type global --csvfile_name OTUS_Image_Classification_F1.csv "
-                        "--dataset_csv_main_path D:/DATA/Pedro_Work/IFREMER_Work/DATA/IFREMER_OTUS/BIGLI_CSVs/ "
-                        "--dataset_main_path /datawork/DATA/SUIM/train_val/train_val/ "
+                        "--dataset_csv_main_path /datawork/pjsotove/DATA/CSVs/OTUS_2018/ "
+                        "--dataset_main_path /datawork/pjsotove/DATA/OTUS_2018_Doneesbrutes_Montsegur/ "
                         "--checkpoints_main_path /datawork/EXPERIMENTS/CHECKPOINTS/")
 if args.phase == 'Test':
     if args.running_in == 'Local_Anaconda_Environment':
