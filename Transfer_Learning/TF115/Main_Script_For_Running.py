3980f6b31c9e23d3144b51f2f17a39ef749deded
pjsoto <pedrosoto423@gmail.com>
2022-03-25 10:36:50 +0100

    Testing different hyperparemeter configuration in the Running Script

--- Transfer_Learning/TF115/Main_Script_For_Running.py
+++ Transfer_Learning/TF115/Main_Script_For_Running.py
@@ -25,10 +25,10 @@ if args.phase == 'train':
         for csv_name in CSV_FILES_NAMES:
             Schedule.append("python " + MAIN_COMMAND + " --train_task Image_Classification --learning_model CNN --backbone_name " + backbone_name + " --pretrained_backbone False --labels_type onehot_labels "
                             "--weights_definition manual --learning_ratedecay True --lr 0.01 --batch_size 5 --epochs 100 --runs 1 --phase train --optimizer MomentumOptimizer "
-                            "--image_rows 4000 --image_cols 6000 --image_channels 3 --new_size_rows 224 --new_size_cols 224 --split_patch True --data_augmentation True --overlap_porcent 0.25 "
+                            "--image_rows 1024 --image_cols 1024 --image_channels 3 --new_size_rows 224 --new_size_cols 224 --split_patch True --data_augmentation True --overlap_porcent 0.25 "
                             "--dataset_name OTUSIFREMER_IMAGELABEL --csvfile_name " + csv_name + " --checkpoint_name " + backbone_name + "/Model_CNN_" + backbone_name + "_" + csv_name + " "
                             "--dataset_csv_main_path /datawork/DATA/CSVs/OTUS_2018/ "
-                            "--dataset_main_path /datawork/DATA/OTUS_2018_Doneesbrutes_Montsegur/ "
+                            "--dataset_main_path /datawork/DATA/OTUS_2018_Doneesbrutes_Montsegur1024/ "
                             "--checkpoints_main_path /datawork/EXPERIMENTS/")
 
 
