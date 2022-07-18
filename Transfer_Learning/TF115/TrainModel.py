50a2d98351c260e641d39ab779811b1e1cc700be
Pedro Juan SOTO VEGA <pjsotove@IFREMER.FR>
2022-07-18 11:36:35 +0200

    Imrproving the continue training section

--- Transfer_Learning/TF115/TrainModel.py
+++ Transfer_Learning/TF115/TrainModel.py
@@ -80,6 +80,7 @@ def main(args):
             continue_ = False
             args.save_checkpoint_path = args.save_checkpoint_path + '/'
         else:
+            args.continue_training = False
             args.initial_epoch = 0
             args.best_val_fs = 0
             args.save_checkpoint_path = args.checkpoint_dir + '/' + args.learning_model + '_' + dt_string + '/'
