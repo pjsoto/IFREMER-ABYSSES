60e85d30c117c80ae1ecead597bcf8fe4a4a620f
pjsoto <pedrosoto423@gmail.com>
2022-08-31 14:55:53 +0200

    Fixing minor issues

--- Transfer_Learning/TF115/Running_Committee_All.py
+++ Transfer_Learning/TF115/Running_Committee_All.py
@@ -1,6 +1,14 @@
 import os
 import sys
 
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--running_in', dest='running_in', type=str, default='Datarmor_PBS', help='Decide wether the script will be running')
+args = parser.parse_args()
+
+if args.running_in == 'Datarmor_Interactive':
+    TestComitee_MAIN_COMMAND = ""
+if args.running_in == 'Datarmor_PBS':
+    TestComitee_MAIN_COMMAND = "$HOME/CODE/IFREMER-ABYSSES/Transfer_Learning/TF115/"
 
 Schedule = []
 
@@ -13,9 +21,9 @@ Schedule = []
 #Schedule.append("python Main_Script_For_Running_Lithology_Comitee_WC.py --cross_domain False")
 #Schedule.append("python Main_Script_For_Running_Lithology_Comitee_ET.py --cross_domain False")
 
-Schedule.append("python Main_Script_For_Running_Morphology_Comitee_MS.py --cross_domain False")
-Schedule.append("python Main_Script_For_Running_Morphology_Comitee_WC.py --cross_domain False")
-Schedule.append("python Main_Script_For_Running_Morphology_Comitee_ET.py --cross_domain False")
+Schedule.append("python " + TestComitee_MAIN_COMMAND + " Main_Script_For_Running_Morphology_Comitee_MS.py --cross_domain False")
+Schedule.append("python " + TestComitee_MAIN_COMMAND + " Main_Script_For_Running_Morphology_Comitee_WC.py --cross_domain False")
+Schedule.append("python " + TestComitee_MAIN_COMMAND + " Main_Script_For_Running_Morphology_Comitee_ET.py --cross_domain False")
 
 for i in range(len(Schedule)):
     os.system(Schedule[i])
